77a9224ebbfeda7ce51b377648582d4214d7e445
Pepijn <pepijn.ten.hoor@student.uva.nl>
2024-06-27 15:26:08 +0200

    fixed script time from 90 seconds to 40 seconds

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,4 +7,4 @@ rm -rf _build
 
 python3 scripts/remove_code_input_cells.py
 jupyter-book build docs #--config ../_config.yml --toc ../_toc.yml
-ghp-import -n -p -f _build/html
+ghp-import -n -p -f docs/_build/html
