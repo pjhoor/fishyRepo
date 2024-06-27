5f7594e305cbda74989a94bd8c13253440840231
Pepijn <pepijn.ten.hoor@student.uva.nl>
2024-06-27 15:20:21 +0200

    changed deploy and story a bit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,5 @@ cd $ROOT_DIR
 rm -rf _build
 
 python3 scripts/remove_code_input_cells.py
-jupyter-book build --all .
+jupyter-book build docs #--config ../_config.yml --toc ../_toc.yml
 ghp-import -n -p -f _build/html
